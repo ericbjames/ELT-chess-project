8f8adaa98787105bda5f8b89d21a26011a4541f8
Eric James <EricBradleyJ@gmail.com>
2023-05-09 16:48:28 -0400

    modified readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# ELT-chess-project
+# CHESS_ELT_PROJECT
+
+## Project Architecture
+
+
+## Data Source
+The pipeline utilizes Lichess API to get chess game data.
+
+## Pipeline Stages
+
+## Dimensional Model:
+
+
+## DBT Lineage:
+
+## Final Dashboard
+
+
+
+
--- warehouse/snowflake/games_elt/models/modeling/one_big_table.sql
+++ warehouse/snowflake/games_elt/models/modeling/one_big_table.sql
@@ -13,26 +13,34 @@ dim_date AS (
   SELECT *
   FROM {{ ref('dim_date') }}
 ),
+fct_game AS (
+  SELECT *
+  FROM {{ ref('fct_game')}}
+),
+dim_result AS (
+  SELECT *
+  FROM {{ ref('dim_result')}}
+),
 
 merged_data AS (
   SELECT 
     my_games_staging.game_key,
-    my_games_staging.clock_increment,
-    my_games_staging.clock_initial,
-    my_games_staging.clock_totaltime,
-    my_games_staging.moves,
-    my_games_staging.perf,
-    my_games_staging.speed,
-    my_games_staging.status,
-    my_games_staging.black_rating,
-    my_games_staging.black_ratingdiff,
-    my_games_staging.black_user_id,
-    my_games_staging.black_user_name,
-    my_games_staging.white_rating,
-    my_games_staging.white_ratingdiff,
-    my_games_staging.white_user_id,
-    my_games_staging.white_user_name,
-    my_games_staging.winner,
+    fct_game.clock_increment,
+    fct_game.clock_initial,
+    fct_game.clock_totaltime,
+    fct_game.moves,
+    fct_game.perf,
+    fct_game.speed,
+    fct_game.status,
+    dim_result.black_rating,
+    dim_result.black_ratingdiff,
+    dim_result.black_user_id,
+    dim_result.black_user_name,
+    dim_result.white_rating,
+    dim_result.white_ratingdiff,
+    dim_result.white_user_id,
+    dim_result.white_user_name,
+    dim_result.winner,
     dim_opening.opening_name,
     dim_opening.opening_eco,
     dim_opening.first_opening_variation,
@@ -46,25 +54,27 @@ merged_data AS (
     dim_date.last_move_at,
     ROW_NUMBER() OVER (PARTITION BY my_games_staging.game_key ORDER BY dim_date.created_at DESC) AS row_num,
     CASE
-      WHEN my_games_staging.black_user_name = 'penguingim1' AND my_games_staging.winner = 'black' THEN 1
-      WHEN my_games_staging.white_user_name = 'penguingim1' AND my_games_staging.winner = 'white' THEN 1
+      WHEN dim_result.black_user_name = 'penguingim1' AND dim_result.winner = 'black' THEN 1
+      WHEN dim_result.white_user_name = 'penguingim1' AND dim_result.winner = 'white' THEN 1
       ELSE 0
     END AS user_win,
     CASE
-      WHEN my_games_staging.status = 'draw' THEN 1
+      WHEN fct_game.status = 'draw' THEN 1
       ELSE 0
     END AS user_draw,
      CASE
-      WHEN my_games_staging.black_user_name = 'penguingim1' THEN black_rating
-      WHEN my_games_staging.white_user_name = 'penguingim1' THEN white_rating
+      WHEN dim_result.black_user_name = 'penguingim1' THEN dim_result.black_rating
+      WHEN dim_result.white_user_name = 'penguingim1' THEN dim_result.white_rating
     END AS user_elo,
        CASE
-      WHEN my_games_staging.black_user_name = 'penguingim1' THEN black_ratingdiff
-      WHEN my_games_staging.white_user_name = 'penguingim1' THEN white_ratingdiff
+      WHEN dim_result.black_user_name = 'penguingim1' THEN dim_result.black_ratingdiff
+      WHEN dim_result.white_user_name = 'penguingim1' THEN dim_result.white_ratingdiff
     END AS user_elo_diff
   FROM my_games_staging
   JOIN dim_opening ON my_games_staging.opening_name = dim_opening.opening_name
   JOIN dim_date ON my_games_staging.created_at = dim_date.created_at
+  JOIN dim_result ON my_games_staging.game_key = dim_result.result_key
+  JOIN fct_game ON my_games_staging.game_key = fct_game.game_key
   WHERE my_games_staging.perf IN ('blitz', 'bullet', 'ultrabullet')
 )
 
